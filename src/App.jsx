eff8c3fb925b9436814c43f8b0c90c036c64eeb0
nacho12091 <ignaciolima12091@gmail.com>
2022-01-16 21:04:02 -0300

    Navegación

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import NotesList from './components/NotesList/NotesList';
 import NoteCreation from './components/NoteCreation/NoteCreation';
 import UserCreation from './components/UserCreation/UserCreation';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
 
 function App() {
   return (
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
     return (
-        <div>
-            Navigation
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    NotesApp
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="ms-auto navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Notes</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/create">Create note</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/user">Create user</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
